02f83c78f7877dcb8b458132d00254f21d20c111
Paul Cochrane <paul@liekut.de>
2014-02-19 11:39:01 +0100

    Replacing single char variable names with more descriptive ones
    Single char variable names aren't good style and thus should be avoided.

--- modules/controller.py
+++ modules/controller.py
@@ -12,17 +12,17 @@ class Controller(Cmd):
 
     # XXX: maybe more descriptive method name?
     def handle(self, model, view):
-        self.v = view
-        self.m = model
-        self.v.print_info("This is trove " + self.m.version)
-        self.v.print_info("Use Ctrl+D to exit, type 'help' or '?' for help.")
-        self.v.print_info("")
-        self.prompt = "(" + self.m.program + ") "
+        self.view = view
+        self.model = model
+        self.view.print_info("This is trove " + self.model.version)
+        self.view.print_info("Use Ctrl+D to exit, type 'help' or '?' for help.")
+        self.view.print_info("")
+        self.prompt = "(" + self.model.program + ") "
 
     # XXX: method name doesn't match what it does
     # XXX: handle has to be called so that this method can be used and tested
     def default(self, line):
-        self.v.print_error("Unknown syntax: %s"%line)
+        self.view.print_error("Unknown syntax: %s"%line)
 
     # XXX: what does this method do??
     def emptyline(self):
@@ -30,34 +30,34 @@ class Controller(Cmd):
 
     # XXX: what is 'arg' and why isn't it used in this method?
     def do_testcolors(self, arg):
-        self.v.print_colors()
+        self.view.print_colors()
 
-    # XXX: what is 's' and why isn't it used in this method?
+    # XXX: what is 'string' and why isn't it used in this method?
     # XXX: why does this method return true?
-    def do_exit(self, s):
-        self.v.print_info("")
+    def do_exit(self, string):
+        self.view.print_info("")
         return True
 
-    # XXX: what is 's' and why isn't it used in this method?
+    # XXX: what is 'string' and why isn't it used in this method?
     # XXX: this is just a repeat of do_exit().  Why is it here?
     # XXX: why does this method return true?  What is this value used for?
-    def do_quit(self, s):
-        self.v.print_info("")
+    def do_quit(self, string):
+        self.view.print_info("")
         return True
 
-    # XXX: what is 's' and why isn't it used in this method?
+    # XXX: what is 'string' and why isn't it used in this method?
     # XXX: why does this method return true?
-    def do_EOF(self, s):
-        self.v.print_info("\n")
+    def do_EOF(self, string):
+        self.view.print_info("\n")
         return True
 
-    # XXX: what is 's' and why isn't it used in this method?
+    # XXX: what is 'string' and why isn't it used in this method?
     # XXX: this is Linux/Unix specific, and could even be shell-specific
-    def do_clear(self, s):
+    def do_clear(self, string):
         os.system("clear")
 
     # XXX: what is 'arg' and why isn't it used in this method?
     def do_help(self, arg):
-        self.v.print_help()
+        self.view.print_help()
 
 # vim: expandtab shiftwidth=4 softtabstop=4
