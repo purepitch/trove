5e0ce37bea1810158042c467632b7a6e802ddbf4
Paul Cochrane <paul@liekut.de>
2014-02-19 11:30:20 +0100

    [view] putting spaces around operators
    This makes the code more readable.

--- modules/view.py
+++ modules/view.py
@@ -5,21 +5,21 @@ import os
 class View():
 
     def __init__(self):
-        colordict ={}
-        colordict["reset"]="\x1b[0m"
-        colordict["bold"]="\x1b[01m"
-        colordict["teal"]="\x1b[36;06m"
-        colordict["turquoise"]="\x1b[36;01m"
-        colordict["fuscia"]="\x1b[35;01m"
-        colordict["purple"]="\x1b[35;06m"
-        colordict["blue"]="\x1b[34;01m"
-        colordict["darkblue"]="\x1b[34;06m"
-        colordict["green"]="\x1b[32;01m"
-        colordict["darkgreen"]="\x1b[32;06m"
-        colordict["yellow"]="\x1b[33;01m"
-        colordict["brown"]="\x1b[33;06m"
-        colordict["red"]="\x1b[31;01m"
-        colordict["darkred"]="\x1b[31;06m"
+        colordict = {}
+        colordict["reset"] = "\x1b[0m"
+        colordict["bold"] = "\x1b[01m"
+        colordict["teal"] = "\x1b[36;06m"
+        colordict["turquoise"] = "\x1b[36;01m"
+        colordict["fuscia"] = "\x1b[35;01m"
+        colordict["purple"] = "\x1b[35;06m"
+        colordict["blue"] = "\x1b[34;01m"
+        colordict["darkblue"] = "\x1b[34;06m"
+        colordict["green"] = "\x1b[32;01m"
+        colordict["darkgreen"] = "\x1b[32;06m"
+        colordict["yellow"] = "\x1b[33;01m"
+        colordict["brown"] = "\x1b[33;06m"
+        colordict["red"] = "\x1b[31;01m"
+        colordict["darkred"] = "\x1b[31;06m"
         self.codes = colordict
 
     def print_info(self, message):
