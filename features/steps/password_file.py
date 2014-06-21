84ef762664aa0ff219098d4b0c85d68311c8e67c
Paul Cochrane <paul@liekut.de>
2014-06-21 10:56:32 +0200

    [features] moving password-related code into password_file.py

--- features/steps/password_file.py
+++ features/steps/password_file.py
@@ -46,6 +46,12 @@ def trove_started_with_empty_password_file(context):
     context.trove = trove
     assert_true(trove.isalive())
 
+@given(u'a default password file exists')
+def default_password_file_exists(context):
+    default_bfe = "passwd.bfe"
+    create_valid_password_file(default_bfe)
+    assert_true(os.path.exists(default_bfe))
+
 @when(u'the master passphrase is entered')
 def enter_master_passphrase(context):
     context.trove.expect('Please enter master passphrase:')
@@ -73,6 +79,39 @@ def create_empty_bcrypt_file(empty_bcrypt_file):
     empty_file = "empty"
     assert_equal(os.system("touch %s" % empty_file), 0)
     bcrypt = pexpect.spawn("bcrypt %s" % empty_file)
+
+    assert_true(bcrypt.isalive())
+
+    assert_equal(bcrypt.expect('Encryption key:'), 0)
+    assert_equal(bcrypt.match.string, 'Encryption key:')
+    assert_equal(bcrypt.sendline("testtest"), 9)
+    assert_equal(bcrypt.expect('Again:'), 0)
+    assert_equal(bcrypt.match.string.strip(), 'Again:')
+    assert_equal(bcrypt.sendline("testtest"), 9)
+    bcrypt.wait()
+    bcrypt.close()
+
+    assert_false(bcrypt.isalive())
+    assert_equal(bcrypt.exitstatus, 0)
+
+def create_valid_password_file(password_bfe):
+    if os.path.exists(password_bfe):
+        os.remove(password_bfe)
+
+    password_file = password_bfe.split('.')[0]
+    password_data = """\
+[ Papa Smurf (Root) ]
+connection_name: papa.smurf.smurf
+user: root
+password: Mh4_l,ifw2as
+help: Mary had a little lamb, its fleece was white as snow
+description: Root access to main smurf computer
+"""
+    password_fh = open(password_file, "w")
+    password_fh.write(password_data)
+    password_fh.close()
+
+    bcrypt = pexpect.spawn("bcrypt %s" % password_file)
 
     assert_true(bcrypt.isalive())
 
