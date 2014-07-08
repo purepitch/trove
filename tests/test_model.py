a17c39bdba06ad6d3af6cb5fb3e824532bfb5b8f
Paul Cochrane <paul@liekut.de>
2014-07-08 08:38:41 +0200

    [tests] Removing the test for get_config() and a missing config file
    The get_config() method isn't actually used anywhere, so it's questionable
    if we want this test and its associated functionality.

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import re
 
 from modules import Model
 
@@ -13,10 +12,5 @@ class TestModel(unittest.TestCase):
     def testGetConfigExpectsConfigFilename(self):
         with self.assertRaises(TypeError):
             self.model.get_config()
-
-    @unittest.skip("should an error be raised with missing config file?")
-    def testGetConfigRaisesErrorOnMissingConfigFile(self):
-        with self.assertRaises(IOError):
-            self.model.get_config(file="missing.conf")
 
 # vim: expandtab shiftwidth=4 softtabstop=4
