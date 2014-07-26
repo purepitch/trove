5f77acdc3172de01a0a9c8a0e11fe092b7da8f2f
Paul Cochrane <paul@liekut.de>
2014-07-26 11:48:10 +0200

    [api] correcting indentation

--- api/conf.py
+++ api/conf.py
@@ -200,8 +200,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'Trove.tex', u'Trove Documentation',
-   u'Andreas Gerdes', 'manual'),
+        ('index', 'Trove.tex', u'Trove Documentation',
+            u'Andreas Gerdes', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -230,8 +230,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'trove', u'Trove Documentation',
-     [u'Andreas Gerdes'], 1)
+        ('index', 'trove', u'Trove Documentation', [u'Andreas Gerdes'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -244,9 +243,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Trove', u'Trove Documentation',
-   u'Andreas Gerdes', 'Trove', 'One line description of project.',
-   'Miscellaneous'),
+        ('index', 'Trove', u'Trove Documentation',
+            u'Andreas Gerdes', 'Trove', 'One line description of project.',
+            'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
